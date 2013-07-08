46abadadd7b85f9c033d8805d391d1abcdb6d4fb
Scott Horvath <horvathsm@yahoo.com>
2013-07-08 04:23:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  buffer = fs.readFileSync('index.html');  
+  response.send(buffer.toString('utf-8',0,buffer.length));
 });
 
 var port = process.env.PORT || 5000;
